223f22f951def193746c3c77ed703fff6442115d
19khaled88 <kkhaled88hasan@gmail.com>
2024-02-02 16:52:17 +0600

    added chart weekly and monthly

---
+++ src/components/Charts/Bar/WeeklyBarChart.tsx
@@ -0,0 +1,82 @@
+'use client'
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+
+const data = [
+  {
+    name: "Trips",
+    uv: 25,
+    pv: 2400,
+    amt: 12,
+  },
+  {
+    name: "Income",
+    uv: 20000,
+    pv: 1398,
+    amt: 25000,
+  },
+  {
+    name: "Expense",
+    uv: 10000,
+    pv: 9800,
+    amt: 10000,
+  },
+];
+
+const getPath = (x: any, y: any, width: any, height: any) => {
+  return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3
+    }
+  ${x + width / 2}, ${y}
+  C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width
+    }, ${y + height}
+  Z`;
+};
+
+const TriangleBar = (props: any) => {
+  const { fill, x, y, width, height } = props;
+
+  return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+};
+
+export default function WeeklyBarChartDemo() {
+  return (
+    <span>
+      <p>Monthly Trips statistics</p>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        title="Weekly Statistics"
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Bar
+          dataKey="uv"
+          fill="#8884d8"
+          shape={<TriangleBar />}
+          label={{ position: "top" }}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+          ))}
+        </Bar>
+      </BarChart>
+    </span>
+  );
+}
