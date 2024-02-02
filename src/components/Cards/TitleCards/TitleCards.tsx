ec3c1e8c84a9055975e8a59bc40dff765ef5e77c
19khaled88 <kkhaled88hasan@gmail.com>
2024-02-02 14:05:51 +0600

    pull from dev and cards to component

---
+++ src/components/Cards/TitleCards/TitleCards.tsx
@@ -0,0 +1,29 @@
+import Subtitle from "../Typography/Subtitle"
+
+function TitleCard({title, children, topMargin, TopSideButtons}:{title:any,children:any,topMargin:any,TopSideButtons:any}){
+    return(
+        <div className={"card w-full p-6 bg-base-100 shadow-xl text-blue-700" + (topMargin || "mt-6")} style={{boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px"}}>
+
+          {/* Title for Card */}
+            <Subtitle styleClass={TopSideButtons ? "inline-block" : ""}>
+              {title}
+
+              {/* Top side button, show only if present */}
+              {
+                  TopSideButtons && <div className="inline-block float-right">{TopSideButtons}</div>
+              }
+            </Subtitle>
+            
+            <div className="divider mt-2"></div>
+        
+            {/** Card Body */}
+            <div className='h-full w-full pb-6 bg-base-100' >
+                {children}
+            </div>
+        </div>
+        
+    )
+}
+
+
+export default TitleCard
