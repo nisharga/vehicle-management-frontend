8e2beaf3a36a133e3556d150a8bb761a8fca2c72
OmarFaruq947 <omarfaruq.bplus.6600@gmail.com>
2024-02-05 10:29:21 +0600

    conflict remove

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -99,7 +99,7 @@ const Notfound = () => {
               <span>4 0 4</span>
             </span>
             <span className="text-gray-500 text-xl">
-              Sorry, We couldn't find what you are looking for!
+              Sorry We could not find what you are looking for!
             </span>
           </div>
         </center>
