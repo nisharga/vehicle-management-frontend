95e00a6e94e090b82ea0deb84acc481ff41d189d
nex-nisharga <kabir@nexlent.com>
2024-02-14 18:39:14 +0600

    vehicle bug fix and delete method emplemnt deploy issue

--- src/components/Forms/UpdateVehicleForm.tsx
+++ src/components/Forms/UpdateVehicleForm.tsx
@@ -29,8 +29,8 @@ const UpdateVehecleForm = ({ updateID }: any) => {
       message.error("Every field must be filled")
     }else{
       const res = await updateVehicle({updateID, updatedData})
-      console.log(res)
-      message.success(`${e} Deleted Sucessfully`); 
+      console.log(updateID, updatedData)
+      message.success(`Updated Sucessfully`); 
     }
   };
   
