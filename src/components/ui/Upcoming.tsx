d0bcff4741e56bd9fb59ff77b55f95e14ee80b00
OmarFaruq947 <omarfaruq.bplus.6600@gmail.com>
2024-02-15 13:13:07 +0600

    comming soon page add

---
+++ src/components/ui/Upcoming.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+const Upcoming = () => {
+  return (
+    <>
+      <div className="">
+        <div className="flex flex-col justify-center text-slate-700 h-screen w-full text-center">
+          <h1 className="text-5xl">
+            We are <b>Almost</b> there!
+          </h1>
+          <p>Coming Soooooooooooooooooooooooooooooon...</p>
+          <p>Stay tuned for something amazing!!!</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Upcoming;
