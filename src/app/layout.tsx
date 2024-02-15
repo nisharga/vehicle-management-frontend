b928c6ebec7db130ed797112a4a12d19ef11e076
OmarFaruq947 <omarfaruq.bplus.6600@gmail.com>
2024-02-15 12:15:34 +0600

    contact page is done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import Providers from "@/lib/Providers";
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
